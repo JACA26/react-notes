f0b32678d0aa18a97b1558e778d37a528d731de5
Alexander Castilla <alexdead2118@gmail.com>
2021-10-03 14:47:32 -0500

    React notes finalized

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -2,16 +2,16 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-
+/* Copy your firebaseConfig Here */
 const firebaseConfig = {
-    apiKey: "AIzaSyCWtuZt8rfw7aCPlr4iQUCKzVG6gv8PUSw",
-    authDomain: "react-courses-58505.firebaseapp.com",
-    databaseURL: "https://react-courses-58505.firebaseio.com",
-    projectId: "react-courses-58505",
-    storageBucket: "react-courses-58505.appspot.com",
-    messagingSenderId: "755657735139",
-    appId: "1:755657735139:web:b02c959a8b89f5c839fed6",
-    measurementId: "G-99Z20YDMM5"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 firebase.initializeApp(firebaseConfig);
 
